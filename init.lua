f5c3a2e3073890570ee7f1af45bb7c89edfdba77
Avi Avinav <aviavinav2004@gmail.com>
2023-05-03 21:17:06 +0530

    add basic options

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
 require "clipboard"
+require "base"
---
+++ lua/base.lua
@@ -0,0 +1,9 @@
+vim.opt.number = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.smarttab = true
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.wrap = true
+vim.opt.wildignore:append { "*/node_modules/*" }
