996afbc7e86dfde149a828a6ffd7d6757920cdc6
Avi Avinav <aviavinav2004@gmail.com>
2023-05-03 22:55:10 +0530

    run stylua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,48 +1,48 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		local lualine = require "lualine"
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    local lualine = require "lualine"
 
-		lualine.setup({
-  options = {
-    theme = "ayu_dark",
-    section_separators = { "", "" },
-    component_separators = { "", "" },
-    disabled_filetypes = {},
-    icons_enabled = true,
-  },
-  sections = {
-    lualine_a = { "mode" },
-    lualine_b = { "branch" },
-    lualine_c = { {
-      "filename",
-      file_status = true,
-      path = 1,
-    } },
-    lualine_x = {
-      {
-        "diagnostics",
-        sources = { "nvim_diagnostic" },
-        symbols = { error = " ", warn = " ", info = " ", hint = " " },
+    lualine.setup {
+      options = {
+        theme = "ayu_dark",
+        section_separators = { "", "" },
+        component_separators = { "", "" },
+        disabled_filetypes = {},
+        icons_enabled = true,
       },
-    },
-    lualine_y = { "progress" },
-    lualine_z = { "location" },
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { {
-      "filename",
-      file_status = true,
-      path = 1,
-    } },
-    lualine_x = { "location" },
-    lualine_y = {},
-    lualine_z = {},
-  },
-  tabline = {},
-  extensions = { "fugitive" },
-})
-end,
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = { {
+          "filename",
+          file_status = true,
+          path = 1,
+        } },
+        lualine_x = {
+          {
+            "diagnostics",
+            sources = { "nvim_diagnostic" },
+            symbols = { error = " ", warn = " ", info = " ", hint = " " },
+          },
+        },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { {
+          "filename",
+          file_status = true,
+          path = 1,
+        } },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      extensions = { "fugitive" },
+    }
+  end,
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,4 +1,4 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v2.x"
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v2.x",
 }
