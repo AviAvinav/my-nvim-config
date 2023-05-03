a81335b15fe291dd7cc875229ff8b40cc36b097d
Avi Avinav <aviavinav2004@gmail.com>
2023-05-03 23:05:47 +0530

    update neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,4 +1,16 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v2.x",
+	config = function()
+		local neo_tree = require"neo-tree"
+
+		neo_tree.setup({
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				}
+			}
+		})
+	end,
 }
