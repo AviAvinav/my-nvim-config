9010225dae21be2d3c8d4b7aac9190f8ce9ab4c4
Avi Avinav <aviavinav2004@gmail.com>
2023-05-04 00:01:09 +0530

    add keymaps and update neo-tree config

---
+++ lua/keymaps.lua
@@ -0,0 +1,32 @@
+local keymap = vim.keymap
+
+-- Increment/Decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- New Tab
+keymap.set("n", "te", ":tabedit<Return>", { silent = true })
+
+-- Split Window
+keymap.set("n", "ss", ":split<Return><C-w>w", { silent = true })
+keymap.set("n", "sv", ":vsplit<Return><C-w>w", { silent = true })
+
+-- Move Window
+keymap.set("n", "<Space>", "<C-w>w")
+keymap.set("", "s<left>", "<C-w>h")
+keymap.set("", "s<up>", "<C-w>k")
+keymap.set("", "s<down>", "<C-w>j")
+keymap.set("", "s<right>", "<C-w>l")
+keymap.set("", "sh", "<C-w>h")
+keymap.set("", "sk", "<C-w>k")
+keymap.set("", "sj", "<C-w>j")
+keymap.set("", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Neo Tree
+keymap.set("n", "<C-t>", ":NeoTreeFocusToggle<Return>", { silent = true })
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,16 +1,19 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v2.x",
-	config = function()
-		local neo_tree = require"neo-tree"
+  config = function()
+    local neo_tree = require "neo-tree"
 
-		neo_tree.setup({
-			filesystem = {
-				filtered_items = {
-					hide_dotfiles = false,
-					hide_gitignored = false,
-				}
-			}
-		})
-	end,
+    neo_tree.setup {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+      window = {
+        position = "right",
+      },
+    }
+  end,
 }
