d4838ffd6fdea5e7c49f38285d231fb01214f487
Avi Avinav <aviavinav2004@gmail.com>
2023-05-04 22:31:48 +0530

    setup lsp

---
+++ lua/plugins/lspkind.lua
@@ -0,0 +1,31 @@
+return {
+  "onsails/lspkind-nvim",
+  config = function()
+    require("lspkind").init {
+      mode = "symbol_text",
+      preset = "codicons",
+      symbol_map = {
+        Text = "",
+        Method = "ƒ",
+        Function = "",
+        Constructor = "",
+        Variable = "",
+        Class = "",
+        Interface = "ﰮ",
+        Module = "",
+        Property = "",
+        Unit = "",
+        Value = "",
+        Enum = "了",
+        Keyword = "",
+        Snippet = "﬌",
+        Color = "",
+        File = "",
+        Folder = "",
+        EnumMember = "",
+        Constant = "",
+        Struct = "",
+      },
+    }
+  end,
+}
