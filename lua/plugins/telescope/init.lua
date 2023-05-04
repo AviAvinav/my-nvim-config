7cdfe90a3d9c0878ae810a1009984d5dda17d25a
Avi Avinav <aviavinav2004@gmail.com>
2023-05-04 16:25:27 +0530

    add action keymaps for telescope file browser

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -4,12 +4,26 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local tl = require "telescope"
+		local fb_actions = require("telescope").extensions.file_browser.actions
 
     tl.setup {
       extensions = {
         file_browser = {
           theme = "dropdown",
           hijack_netrw = true,
+					mappings = {
+          ["n"] = {
+            ["N"] = fb_actions.create,
+            ["h"] = fb_actions.goto_parent_dir,
+            -- ["/"] = function()
+            --   vim.cmd("startinsert")
+            -- end,
+          },
+        },
+				initial_mode = "normal",
+				layout_config = {
+					height = 40,
+				}
         },
       },
     }
