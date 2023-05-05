5ad143c88b05a1569bca76be2c86aee6f67b1562
Avi Avinav <aviavinav2004@gmail.com>
2023-05-05 18:22:42 +0530

    enable tokyonight theme with transparent background

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -5,9 +5,9 @@ vim.opt.wildoptions = "pum"
 vim.opt.pumblend = 5
 vim.opt.background = "dark"
 
-vim.cmd [[ hi Cursorline guibg=#1e222a ]]
-vim.cmd [[ hi Cursor guibg=#000000 ]]
-vim.cmd [[ hi Cursor guifg=#1e222a ]]
-
-vim.cmd [[ hi Pmenu guibg=#191e29 ]]
-vim.cmd [[ hi PmenuSel guibg=#2f394f ]]
+-- vim.cmd [[ hi Cursorline guibg=#1e222a ]]
+-- vim.cmd [[ hi Cursor guibg=#000000 ]]
+-- vim.cmd [[ hi Cursor guifg=#1e222a ]]
+--
+-- vim.cmd [[ hi Pmenu guibg=#191e29 ]]
+-- vim.cmd [[ hi PmenuSel guibg=#2f394f ]]
