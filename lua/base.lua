b2dde3543396534967d6a265cc09e1fe0c95668c
Avi Avinav <aviavinav2004@gmail.com>
2023-05-05 12:51:31 +0530

    disable wrap

--- lua/base.lua
+++ lua/base.lua
@@ -5,5 +5,5 @@ vim.opt.softtabstop = 2
 vim.opt.smarttab = true
 vim.opt.showcmd = true
 vim.opt.cmdheight = 1
-vim.opt.wrap = true
+vim.opt.wrap = false
 vim.opt.wildignore:append { "*/node_modules/*" }
