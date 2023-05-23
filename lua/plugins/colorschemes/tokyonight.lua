e349846e0706189347ebbac3cdad47818998ed9c
Avi Avinav <aviavinav2004@gmail.com>
2023-05-23 22:44:30 +0530

    update tokyonight config, make telescope borderless

--- lua/plugins/colorschemes/tokyonight.lua
+++ lua/plugins/colorschemes/tokyonight.lua
@@ -1,24 +1,53 @@
 return {
-  "folke/tokyonight.nvim",
-  config = function()
-    local tn = require "tokyonight"
+	"folke/tokyonight.nvim",
+	config = function()
+		local tn = require "tokyonight"
+		local prompt = "#2d3149"
+		tn.setup {
+			style = "night",
+			transparent = true,
+			styles = {
+				-- Style to be applied to different syntax groups
+				-- Value is any valid attr-list value for `:help nvim_set_hl`
+				comments = { italic = true },
+				keywords = { italic = true },
+				functions = {},
+				variables = {},
+				-- Background styles. Can be "dark", "transparent" or "normal"
+				sidebars = "dark", -- style for sidebars, see below
+				floats = "dark",   -- style for floating windows
+			},
+			on_highlights = function(hl, c)
+				hl.TelescopeNormal = {
+					bg = c.bg_dark,
+					fg = c.fg_dark,
+				}
+				hl.TelescopeBorder = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+				hl.TelescopePromptNormal = {
+					bg = prompt,
+				}
+				hl.TelescopePromptBorder = {
+					bg = prompt,
+					fg = prompt,
+				}
+				hl.TelescopePromptTitle = {
+					bg = prompt,
+					fg = prompt,
+				}
+				hl.TelescopePreviewTitle = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+				hl.TelescopeResultsTitle = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+			end,
+		}
 
-    tn.setup {
-      style = "night",
-      transparent = true,
-      styles = {
-        -- Style to be applied to different syntax groups
-        -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = {},
-        variables = {},
-        -- Background styles. Can be "dark", "transparent" or "normal"
-        sidebars = "dark", -- style for sidebars, see below
-        floats = "dark", -- style for floating windows
-      },
-    }
-
-    vim.cmd "colorscheme tokyonight"
-  end,
+		vim.cmd "colorscheme tokyonight"
+	end,
 }
