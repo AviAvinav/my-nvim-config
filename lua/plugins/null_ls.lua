fbb0c454051cd26e1110f7e0b7d735bc7a53c58e
Avi Avinav <aviavinav2004@gmail.com>
2023-06-16 22:48:04 +0530

    try out new colorschemes and add rustfmt to null-ls

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -1,35 +1,36 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  event = { "BufReadPre", "BufWritePre" },
-  config = function()
-    local null_ls = require "null-ls"
+	"jose-elias-alvarez/null-ls.nvim",
+	event = { "BufReadPre", "BufWritePre" },
+	config = function()
+		local null_ls = require "null-ls"
 
-    local formatting = null_ls.builtins.formatting
-    local diagnostics = null_ls.builtins.diagnostics
+		local formatting = null_ls.builtins.formatting
+		local diagnostics = null_ls.builtins.diagnostics
 
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    null_ls.setup {
-      on_attach = function(client, bufnr)
-        if client.supports_method "textDocument/formatting" then
-          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-              vim.lsp.buf.format { bufnr = bufnr }
-            end,
-          })
-        end
-      end,
-      sources = {
-        formatting.prettierd.with {
-          extra_filetypes = { "md", "mdx" },
-        },
-        formatting.stylua,
-        -- diagnostics.eslint,
-      },
-    }
-  end,
+		null_ls.setup {
+			on_attach = function(client, bufnr)
+				if client.supports_method "textDocument/formatting" then
+					vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+							vim.lsp.buf.format { bufnr = bufnr }
+						end,
+					})
+				end
+			end,
+			sources = {
+				formatting.prettierd.with {
+					extra_filetypes = { "md", "mdx" },
+				},
+				formatting.stylua,
+				-- diagnostics.eslint,
+				formatting.rustfmt,
+			},
+		}
+	end,
 }
--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -1,23 +1,23 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		cmd = "Telescope",
-	},
-	{
-		"nvim-telescope/telescope-file-browser.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-		event = { "VeryLazy" },
-		config = function()
-			local tl = require "telescope"
-			local file_browser = require "plugins.telescope.file_browser"
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = "Telescope",
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    event = { "VeryLazy" },
+    config = function()
+      local tl = require "telescope"
+      local file_browser = require "plugins.telescope.file_browser"
 
-			tl.setup {
-				extensions = {
-					file_browser = file_browser,
-				},
-			}
-			tl.load_extension "file_browser"
-		end,
-	},
+      tl.setup {
+        extensions = {
+          file_browser = file_browser,
+        },
+      }
+      tl.load_extension "file_browser"
+    end,
+  },
 }
