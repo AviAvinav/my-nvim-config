a2cbaee1c6a72c7481b4f6589ec1fdd6074c147c
Avi Avinav <aviavinav2004@gmail.com>
2023-06-20 21:36:40 +0530

    tried a few things with colorschemes

--- lua/plugins/colorschemes/init.lua
+++ lua/plugins/colorschemes/init.lua
@@ -1,4 +1,5 @@
 return {
   require "plugins.colorschemes.tokyonight",
   require "plugins.colorschemes.poimandres",
+  require "plugins.colorschemes.t2c",
 }
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,101 +1,102 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local nvim_lsp = require "lspconfig"
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local nvim_lsp = require "lspconfig"
 
-      local on_attach = function(client, bufnr)
-        local function buf_set_keymap(...)
-          vim.api.nvim_buf_set_keymap(bufnr, ...)
-        end
-        local function buf_set_option(...)
-          vim.api.nvim_buf_set_option(bufnr, ...)
-        end
+			local on_attach = function(client, bufnr)
+				local function buf_set_keymap(...)
+					vim.api.nvim_buf_set_keymap(bufnr, ...)
+				end
+				local function buf_set_option(...)
+					vim.api.nvim_buf_set_option(bufnr, ...)
+				end
 
-        -- Enable completion triggered by <c-x><c-o>
-        buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+				-- Enable completion triggered by <c-x><c-o>
+				buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-        -- Mappings.
-        local opts = { noremap = true, silent = true }
+				-- Mappings.
+				local opts = { noremap = true, silent = true }
 
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-        buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-        buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-        buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-        buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-        buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-        buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-        buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-        buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-        buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-        buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-        buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-        buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-        buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-      end
+				-- See `:help vim.lsp.*` for documentation on any of the below functions
+				buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+				buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+				buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+				buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+				buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+				buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+				buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+				buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+				buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+				buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+				buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+				buf_set_keymap("n", "<space>re", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+				buf_set_keymap("n", "yt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+				buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+				buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+			end
 
-      nvim_lsp.tsserver.setup {
-        on_attach = on_attach,
-      }
+			nvim_lsp.tsserver.setup {
+				on_attach = on_attach,
+			}
 
-      nvim_lsp.lua_ls.setup {
-        on_attach = on_attach,
-        settings = {
-          Lua = {
-            diagnostics = {
-              -- Get the language server to recognize the `vim` global
-              globals = { "vim" },
-            },
-            workspace = {
-              -- Make the server aware of Neovim runtime files
-              library = vim.api.nvim_get_runtime_file("", true),
-            },
-          },
-        },
-      }
+			nvim_lsp.lua_ls.setup {
+				on_attach = on_attach,
+				settings = {
+					Lua = {
+						diagnostics = {
+							-- Get the language server to recognize the `vim` global
+							globals = { "vim" },
+						},
+						workspace = {
+							-- Make the server aware of Neovim runtime files
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+					},
+				},
+			}
 
-      nvim_lsp.astro.setup {
-        on_attach = on_attach,
-      }
+			nvim_lsp.astro.setup {
+				on_attach = on_attach,
+			}
 
-      nvim_lsp.emmet_ls.setup {
-        on_attach = on_attach,
-      }
+			nvim_lsp.emmet_ls.setup {
+				on_attach = on_attach,
+			}
 
-      nvim_lsp.tailwindcss.setup {
-        on_attach = on_attach,
-      }
+			nvim_lsp.tailwindcss.setup {
+				on_attach = on_attach,
+			}
 
-      nvim_lsp.gopls.setup {
-        on_attach = on_attach,
-      }
+			nvim_lsp.gopls.setup {
+				on_attach = on_attach,
+			}
 
-      nvim_lsp.rust_analyzer.setup {
-        on_attach = on_attach,
-      }
+			nvim_lsp.rust_analyzer.setup {
+				on_attach = on_attach,
+			}
 
-      nvim_lsp.pyright.setup {
-        on_attach = on_attach,
-      }
+			nvim_lsp.pyright.setup {
+				on_attach = on_attach,
+			}
 
-      nvim_lsp.vuels.setup {
-        on_attach = on_attach,
-      }
+			nvim_lsp.vuels.setup {
+				on_attach = on_attach,
+			}
 
-      nvim_lsp.zls.setup {
-        on_attach = on_attach,
-      }
+			nvim_lsp.zls.setup {
+				on_attach = on_attach,
+			}
 
-      nvim_lsp.svelte.setup {
-        on_attach = on_attach,
-      }
+			nvim_lsp.svelte.setup {
+				on_attach = on_attach,
+			}
 
-      nvim_lsp.eslint.setup {
-        on_attach = on_attach,
-      }
-    end,
-  },
-  require "plugins.lsp.lspkind",
+			nvim_lsp.eslint.setup {
+				on_attach = on_attach,
+			}
+		end,
+	},
+	require "plugins.lsp.lspkind",
 }
