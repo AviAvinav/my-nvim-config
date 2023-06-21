50d1d7d06dbd6db0a699dc349ff9652c3340f3c8
Avi Avinav <aviavinav2004@gmail.com>
2023-06-21 10:11:35 +0530

    add keybinding to open floating diagnostics

--- lua/plugins/colorschemes/t2c.lua
+++ lua/plugins/colorschemes/t2c.lua
@@ -1,45 +1,45 @@
 return {
-  "svermeulen/text-to-colorscheme.nvim",
-  config = function()
-    require("text-to-colorscheme").setup {
-      ai = {
-        openai_api_key = "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",
-        gpt_model = "gpt-3.5-turbo",
-      },
-      hex_palettes = {
-        {
-          name = "darkest knight batman",
-          background_mode = "dark",
-          background = "#1c1c1c",
-          foreground = "#f8f8f2",
-          accents = {
-            "#bd93f9",
-            "#ff79c6",
-            "#8be9fd",
-            "#ffb86c",
-            "#44d469",
-            "#6272a4",
-            "#ff5555",
-          },
-        },
-        {
-          name = "diwali dark",
-          background_mode = "dark",
-          background = "#1c1c1c",
-          foreground = "#d4d4d4",
-          accents = {
-            "#f4c110",
-            "#f45e1b",
-            "#267f63",
-            "#2d7cd1",
-            "#b4009e",
-            "#f4005f",
-            "#00a0b4",
-          },
-        },
-      },
-      -- default_palette = "tokyonight",
-    }
-    -- vim.cmd [[colorscheme text-to-colorscheme]]
-  end,
+	"svermeulen/text-to-colorscheme.nvim",
+	config = function()
+		require("text-to-colorscheme").setup {
+			ai = {
+				openai_api_key = "",
+				gpt_model = "gpt-3.5-turbo",
+			},
+			hex_palettes = {
+				{
+					name = "darkest knight batman",
+					background_mode = "dark",
+					background = "#1c1c1c",
+					foreground = "#f8f8f2",
+					accents = {
+						"#bd93f9",
+						"#ff79c6",
+						"#8be9fd",
+						"#ffb86c",
+						"#44d469",
+						"#6272a4",
+						"#ff5555",
+					},
+				},
+				{
+					name = "diwali dark",
+					background_mode = "dark",
+					background = "#1c1c1c",
+					foreground = "#d4d4d4",
+					accents = {
+						"#f4c110",
+						"#f45e1b",
+						"#267f63",
+						"#2d7cd1",
+						"#b4009e",
+						"#f4005f",
+						"#00a0b4",
+					},
+				},
+			},
+			-- default_palette = "tokyonight",
+		}
+		-- vim.cmd [[colorscheme text-to-colorscheme]]
+	end,
 }
