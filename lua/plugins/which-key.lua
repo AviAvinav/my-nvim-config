7a32e03e4aeb13f264bf84490bfb011062946151
Avi Avinav <aviavinav2004@gmail.com>
2024-01-02 21:41:37 +0530

    change which-key timeout to make it less annoying

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,11 +2,12 @@ local M = {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
 	opts = {},
+	cmd = { "WhichKey" },
 }
 
 M.init = function()
 	vim.o.timeout = true
-	vim.o.timeoutlen = 300
+	vim.o.timeoutlen = 6000
 end
 
 return M
