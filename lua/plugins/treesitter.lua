a06694efb09244c752c455bdafd3a081b981d96a
Avi Avinav <aviavinav2004@gmail.com>
2024-01-10 23:22:41 +0530

    run stylua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,32 +1,32 @@
 local M = {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
 }
 
 M.opts = {
-	autotag = {
-		enable = true,
-	},
-	ensure_installed = {
-		"css",
-		"html",
-		"javascript",
-		"json",
-		"lua",
-		"python",
-		"rust",
-		"tsx",
-		"typescript",
-		"yaml",
-		"vue",
-		"svelte",
-		"astro",
-		"c",
-		"cpp",
-		"go",
-		"haskell",
-	},
+  autotag = {
+    enable = true,
+  },
+  ensure_installed = {
+    "css",
+    "html",
+    "javascript",
+    "json",
+    "lua",
+    "python",
+    "rust",
+    "tsx",
+    "typescript",
+    "yaml",
+    "vue",
+    "svelte",
+    "astro",
+    "c",
+    "cpp",
+    "go",
+    "haskell",
+  },
 }
 
 return M
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,13 +1,13 @@
 local M = {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts = {},
-	cmd = { "WhichKey" },
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {},
+  cmd = { "WhichKey" },
 }
 
 M.init = function()
-	vim.o.timeout = true
-	vim.o.timeoutlen = 6000
+  vim.o.timeout = true
+  vim.o.timeoutlen = 6000
 end
 
 return M
