7b2f29b6c97a1335ebff96930983c1d1d31ca051
Avi Avinav <aviavinav2004@gmail.com>
2024-08-05 22:20:35 +0530

    add mason-lspconfig, autosetup lsp servers

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -16,29 +16,65 @@ return {
     cmd = { "MasonUpdate", "MasonInstall", "Mason" },
   },
   {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
-      require("mason-tool-installer").setup {
+      local on_attach = function(_, bufnr)
+        local function buf_set_keymap(...)
+          vim.api.nvim_buf_set_keymap(bufnr, ...)
+        end
+
+        local opts = { noremap = true, silent = true }
+
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+        buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+        buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+        buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+        buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+        buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+        buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+        buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+        buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+        buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+        buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+        buf_set_keymap("n", "<space>re", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+        buf_set_keymap("n", "Y", "<cmd>lua vim.diagnostic.open_float(0, { scope = 'line' })<CR>", opts)
+        buf_set_keymap("n", "yt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+        buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+        buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+      end
+
+      require("mason-lspconfig").setup {
         ensure_installed = {
-          "lua-language-server",
-          "typescript-language-server",
           "gopls",
-          "stylua",
-          "tailwindcss-language-server",
-          "astro-language-server",
-          "vue-language-server",
-          "svelte-language-server",
           "pyright",
-          "rust-analyzer",
-          "emmet-ls",
-          "eslint-lsp",
-          "rustfmt",
-          "prettier",
-          "prettierd",
+          "zls",
+          "clangd",
         },
       }
+      require("mason-lspconfig").setup_handlers {
+        function(server)
+          require("lspconfig")[server].setup {
+            on_attach = on_attach,
+          }
+        end,
+        ["lua_ls"] = function()
+          require("lspconfig").lua_ls.setup {
+            on_attach = on_attach,
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" },
+                },
+                workspace = {
+                  library = vim.api.nvim_get_runtime_file("", true),
+                },
+              },
+            },
+          }
+        end,
+      }
     end,
-    dependencies = { "williamboman/mason.nvim" },
-    cmd = { "MasonToolsInstall", "MasonToolsUpdate" },
   },
 }
